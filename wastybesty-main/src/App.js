25980cccbe67b03d0f46b19cfdcba68d97fd98b5
AbhayG9119 <140013541+AbhayG9119@users.noreply.github.com>
2025-08-29 08:46:46 +0530

    Add files via upload

--- wastybesty-main/src/App.js
+++ wastybesty-main/src/App.js
@@ -3,20 +3,23 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ScrollTopButton from "./components/ScrollTopButton";
+import ScrollToTop from "./components/ScrollToTop";
 
 import Home from "./pages/Home";
 import Videos from "./pages/Videos";
 import Shorts from "./pages/Shorts";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import FAQ from "./pages/FAQ";
 import Blogs from "./pages/Blog";
+import BestyBuilds from "./pages/BestyBuilds";
+import PlantCareGuide from "./pages/PlantCareGuide";
 
 export default function App() {
   useEffect(() => { document.title = "Wasty Besty • Official Website"; }, []);
 
   return (
     <Router>
+      <ScrollToTop />
       <Navbar />
       <main>
         <Routes>
@@ -25,8 +28,9 @@ export default function App() {
           <Route path="/shorts" element={<Shorts />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="/blog" element={<Blogs />} />  
+          <Route path="/blog" element={<Blogs />} />
+          <Route path="/bestybuilds" element={<BestyBuilds />} />
+          <Route path="/plantcareguide" element={<PlantCareGuide />} />
         </Routes>
       </main>
       <Footer />
